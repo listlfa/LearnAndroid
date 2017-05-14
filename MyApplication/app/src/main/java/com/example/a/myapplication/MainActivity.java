2e46abfaec1ffffc623e14d508fcb28a33a863bf
listlfa <list_lfa@yahoo.com>
2017-05-14 12:31:09 +0800

    Finished Step 2
    Finished
    https://developer.android.com/training/basics/firstapp/starting-activity.html#run

--- MyApplication/app/src/main/java/com/example/a/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/a/myapplication/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.a.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "com.example.a.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,7 +17,12 @@ public class MainActivity extends AppCompatActivity {
 
     /** Called when the user taps the Send button */
     public void sendMessage(View view) {
-        // Do something in response to button
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+
     }
 
 }
